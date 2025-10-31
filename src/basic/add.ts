b75121977daed56b4e5b81e8136aff9dfac0ebbd
Vladimir Solo <freeman.wushu@mail.ru>
2025-10-31 20:16:45 +0300

    refactor: use Streams API for add comand

--- src/basic/add.ts
+++ src/basic/add.ts
@@ -1,12 +1,20 @@
 import * as fs from 'fs';
-import { currentDirectory } from "../utils";
+import { currentDirectory, invalidCommandMsg } from "../utils";
 
 export const add = async (fileName: string): Promise<void> => {
   try {
-    await fs.promises.writeFile(fileName, '');
+    const writeStream = fs.createWriteStream(fileName);
+
+    await new Promise<void>((resolve, reject) => {
+      writeStream.on('finish', resolve);
+      writeStream.on('error', reject);
+      writeStream.end();
+    });
+
+    console.log('\x1b[32m%s\x1b[0m', 'File created successfully.');
     currentDirectory();
   } catch (error: any) {
     console.log('\x1b[31m%s\x1b[0m', `Error creating file: ${error.message}`);
+    invalidCommandMsg();
   }
 };
-
