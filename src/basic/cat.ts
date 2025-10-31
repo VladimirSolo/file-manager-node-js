b9986500581717bb5e9ee2fa3732470550bb65d9
Vladimir Solo <freeman.wushu@mail.ru>
2025-10-31 20:16:58 +0300

    refactor: use Streams API for cat comand

--- src/basic/cat.ts
+++ src/basic/cat.ts
@@ -1,19 +1,22 @@
 import * as fs from 'fs';
 import * as readline from 'readline';
-import { currentDirectory } from "../utils";
+import { currentDirectory, invalidCommandMsg } from "../utils";
 
 export const cat = (filePath: string): void => {
-  try {
-    const readStream = fs.createReadStream(filePath);
-    const rl = readline.createInterface({
-      input: readStream,
-      output: process.stdout,
-    });
+  const readStream = fs.createReadStream(filePath);
 
-    rl.on('close', () => {
-      currentDirectory();
-    });
-  } catch (error: any) {
+  readStream.on('error', (error: any) => {
     console.log('\x1b[31m%s\x1b[0m', `Error reading file: ${error.message}`);
-  }
+    invalidCommandMsg();
+  });
+
+  const rl = readline.createInterface({
+    input: readStream,
+    output: process.stdout,
+    terminal: false
+  });
+
+  rl.on('close', () => {
+    currentDirectory();
+  });
 };
