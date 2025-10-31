b61034c2dbea95feffa07c981e9b1b237eb02f70
Vladimir Solo <freeman.wushu@mail.ru>
2025-10-31 20:15:18 +0300

    feat: add console msg console to rename and remove

--- src/basic/remove.ts
+++ src/basic/remove.ts
@@ -4,6 +4,7 @@ import { currentDirectory, invalidCommandMsg } from "../utils";
 export const remove = async (filePath: string): Promise<void> => {
   try {
     await fs.promises.unlink(filePath);
+    console.log('\x1b[32m%s\x1b[0m', 'File deleted successfully.');
     currentDirectory();
   } catch (error: any) {
     console.log('\x1b[31m%s\x1b[0m', `Error deleting file: ${error.message}`);
--- src/basic/rename.ts
+++ src/basic/rename.ts
@@ -1,11 +1,13 @@
 import * as fs from 'fs';
-import { currentDirectory } from '../utils';
+import { currentDirectory, invalidCommandMsg } from '../utils';
 
 export const rename = async (oldPath: string, newFileName: string): Promise<void> => {
   try {
     await fs.promises.rename(oldPath, newFileName);
+    console.log('\x1b[32m%s\x1b[0m', 'File renamed successfully.');
     currentDirectory();
   } catch (error: any) {
     console.log('\x1b[31m%s\x1b[0m', `Error renaming file: ${error.message}`);
+    invalidCommandMsg();
   }
 };
